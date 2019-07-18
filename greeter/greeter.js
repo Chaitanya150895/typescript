9edeb82cabf0b4ccddce121a073f0701f8cef401
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-18 17:33:07 +0530

    block-scoped variables can’t be read or written to before they’re actually declared.

--- greeter/greeter.js
+++ greeter/greeter.js
@@ -1,9 +1,6 @@
-function f(input) {
-    var a = 100;
-    if (input) {
-        //still okay to refence 'a'
-        var b = a + 1;
-        return b;
-    }
-    // Error 'b' doesn't exist here
+try {
+    throw "oh no!";
+}
+catch (e) {
+    console.log("Oh well.");
 }
--- greeter/greeter.ts
+++ greeter/greeter.ts
@@ -1,12 +1,6 @@
-function f(input: boolean){
-    let a = 100;
-
-    if (input) {
-        //still okay to refence 'a'
-        let b = a + 1;
-        return b;
-    }
-
-    // Error 'b' doesn't exist here
-   
+try {
+    throw "oh no!";
+}
+catch (e) {
+    console.log("Oh well.");
 }
